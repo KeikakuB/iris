fe40139eb039512b67e06bc3018b76b4c466b576
Bill Christian Tyros <me@btyros.com>
2019-06-15 12:18:41 -0400

    Add CLI argument to set a maximum number of articles to get from each source

--- generate_news_poem.sh
+++ generate_news_poem.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-iris -s data/sources.txt > out/news.txt
+iris -s data/sources.txt -n 20 > out/news.txt
 cat out/news.txt | poet poem > out/poem.txt
 cat out/poem.txt
--- iris.py
+++ iris.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import logging
 import sys
 import os
@@ -9,8 +7,9 @@ import newspaper
 
 @click.command()
 @click.option('-s', '--sources', type=click.Path(exists=True), help="File containing news source URLs on each line, defaults to taking them from stdin if not set.")
+@click.option('-n', '--max_articles_per_source', type=int, default=None, help="Maximum number of articles to pull from each source.")
 @click.option('-v', '--verbose', is_flag=True, help="Enable verbose logging.")
-def cli(sources, verbose):
+def cli(sources, verbose, max_articles_per_source):
     logging_level = logging.DEBUG if verbose else logging.ERROR
     logging.basicConfig(stream=sys.stderr, level=logging_level)
 
@@ -21,12 +20,17 @@ def cli(sources, verbose):
     sources = lines
     if f is not sys.stdin:
         f.close()
+
     for paper_url in sources:
         paper = newspaper.build(paper_url)
+        count = 0
         for article in paper.articles:
+            if max_articles_per_source and count > max_articles_per_source:
+                break
             try:
                 article.download()
                 article.parse()
                 print(article.text)
+                count += 1
             except:
                 pass
