a3c2eb7734b8a731eb7a42b2db05384e32f65226
Bill Christian Tyros <me@btyros.com>
2019-06-15 21:59:14 -0400

    Fix haikus being posted on one line with no spaces

--- twit.py
+++ twit.py
@@ -14,5 +14,5 @@ ACCESS_KEY = data['twitter']['ACCESS_KEY']
 ACCESS_SECRET = data['twitter']['ACCESS_SECRET']
 api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 lines = [l.strip() for l in fileinput.input()]
-message = ''.join(lines)
+message = '\n'.join(lines)
 api.update_status(status=message)
